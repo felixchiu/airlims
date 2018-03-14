def220feed3100192edab64142bce4cd19c9b58e
Felix Chiu <felix.chiu@gmail.com>
2018-03-14 13:42:55 -0700

    Initial Readme.

---
+++ src/main/java/com/airlims/enterprise/applications/ApplicationsApplication.java
@@ -0,0 +1,12 @@
+package com.airlims.enterprise.applications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationsApplication.class, args);
+	}
+}
